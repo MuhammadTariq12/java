69c45e4bd4b198420a3d1578bd3aae91ece58e88
Hafiz Muhammad Tariq <hafizmuhammadtahir12@gmail.com>
2023-06-08 16:52:53 +0500

    initialcode

---
+++ javascriptr1/app.js
@@ -0,0 +1,27 @@
+//let name = "tariqmajeed";
+//variable //var //let
+//variable Rules
+// legar or illegal variable
+//use cmle case for long variables name
+//let lastName //camelCase
+// alowed special chararcter _$ only
+//nothing start variable in number 
+//varaible use only camelCase
+//illegel variale
+//1 e.g tariq najeed khan //spaces not allowed
+
+//2 tari#majeed
+//3javascript ka reserveed word sa bi ham variable ni bna skta
+
+alert(name);
+
+//datatypes
+ // number 
+ let muNumber = 9;
+// example  1,4,45,44. alll is number
+//string 
+let name = "tariqmajeed";
+//example name sentences special character all string in javascript
+//boolean
+let isTodayMonday =true;
+//example true& false
